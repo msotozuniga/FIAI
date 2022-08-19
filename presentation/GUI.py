412cc2837c3ce5887cf72438670735c169ce67c5
matias <matias.soto.zuniga@gmail.com>
2022-08-19 17:11:50 -0400

    Preparando GUI para layout

--- presentation/GUI.py
+++ presentation/GUI.py
@@ -4,59 +4,13 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
-class MainWindo1w(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("My App")
-
-        label = QLabel("Hello!")
-        label.setAlignment(Qt.AlignCenter)
-
-        self.setCentralWidget(label)
-
-        toolbar = QToolBar("My main toolbar")
-        toolbar.setIconSize(QSize(16, 16))
-        self.addToolBar(toolbar)
-
-        button_action = QAction(QIcon("bug.png"), "&Your button", self)
-        button_action.setStatusTip("This is your button")
-        button_action.triggered.connect(self.onMyToolBarButtonClick)
-        button_action.setCheckable(True)
-        toolbar.addAction(button_action)
-
-        toolbar.addSeparator()
-
-        button_action2 = QAction(QIcon("bug.png"), "Your &button2", self)
-        button_action2.setStatusTip("This is your button2")
-        button_action2.triggered.connect(self.onMyToolBarButtonClick)
-        button_action2.setCheckable(True)
-        toolbar.addAction(button_action2)
-
-        toolbar.addWidget(QLabel("Hello"))
-        toolbar.addWidget(QCheckBox())
-
-        self.setStatusBar(QStatusBar(self))
-
-        menu = self.menuBar()
-
-        file_menu = menu.addMenu("&File")
-        file_menu.addAction(button_action)
-        file_menu.addSeparator()
-
-        file_submenu = file_menu.addMenu("Submenu")
-        file_submenu.addAction(button_action2)
-
-    def onMyToolBarButtonClick(self, s):
-        print("click", s)
 
 class Mainwindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("FIAI")
-        widget = QWidget()
-        self.setCentralWidget(widget)
         self.createMenu()
+        self.createWidget()
 
         self.setMinimumSize(720, 480)
         self.setMaximumSize(1920,1080)
@@ -74,6 +28,13 @@ class Mainwindow(QMainWindow):
         file_menu.addAction(save_action)
         file_menu.addAction(close_action)
         self.setMenuBar(self.menu)
+
+
+    def createWidget(self):
+        widget = QWidget()
+        main_layout = QHBoxLayout()
+        #TODO continuar
+        self.setCentralWidget(widget)
         
 
     def openFile(self):
